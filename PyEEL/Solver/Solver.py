071fb6e9d462c89c432796b8c9f53519bb5393cb
HotShot0901 <contact.hot.shor.1234@gmail.com>
2026-02-15 23:37:37 +0530

    feature: implement NumpySolver class for linear equation solving using NumPy

--- PyEEL/Solver/Solver.py
+++ PyEEL/Solver/Solver.py
@@ -4,3 +4,8 @@ import numpy as np
 class LinearSolver(ABC):
     @abstractmethod
     def Solve(self, matrix: np.ndarray, vector: np.ndarray) -> np.ndarray: ...
+    
+# Common solvers
+class NumpySolver(LinearSolver):
+    def Solve(self, matrix: np.ndarray, vector: np.ndarray) -> np.ndarray:
+        return np.linalg.solve(matrix, vector)
